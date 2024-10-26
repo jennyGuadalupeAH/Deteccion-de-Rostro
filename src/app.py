d08fbfc8c8cd7d54ce1b38b687003d98e5025922
Jennifer Guadalupe Angeles Hernandez <jennyangeleshernandez310@gmail.com>
2024-10-25 20:59:14 -0600

    UPDATE: new config

--- src/app.py
+++ src/app.py
@@ -35,4 +35,6 @@ def detectar_Puntos_Faciales():
       return jsonify(resultado)
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   if not os.path.exists(app.config['UPLOAD_FOLDER']):
+      os.makedirs(app.config['UPLOAD_FOLDER'])
+   app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
